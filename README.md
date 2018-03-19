d865e31de0d868d40903947636aa9fc621916ad6
Wang Qun Feng <qunfengw@gmail.com>
2018-03-19 20:07:35 +0800

    the final project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # github-battle
 
 `npm run start` to let the fun begin!
+
+[https://github-battle-f5cf9.firebaseapp.com](https://github-battle-f5cf9.firebaseapp.com)
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,17 +1,15 @@
-var React = require("react");
-var Link = require("react-router-dom").Link;
+var React = require('react');
+var Link = require('react-router-dom').Link;
 
 class Home extends React.Component {
-    render() {
-        return (
-                <div className="home-container">
-                <h1>Battle your friends...</h1>
-                <Link className="button" to="/battle">
-                    Battle
-                </Link>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='home-container'>
+        <h1>Github Battle: Battle your friends... and stuff.</h1>
+        <Link className='button' to='/battle'>Battle</Link>
+      </div>
+    )
+  }
 }
 
 module.exports = Home;
